aa820ca8434fa26dbf72ed9083c68fa90a0e5591
Quetan <quetancraft@gmail.com>
2023-10-30 15:35:01 +0700

    Add Swagger API.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,30 +2,37 @@ import { Body, Controller, HttpCode, Post, UsePipes, ValidationPipe } from '@nes
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
 import { RefreshTokenDto } from './dto/refresh-token.dto';
+import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
 	constructor(private readonly authService: AuthService) {}
 
 	/**
-	 * 	Register, createTokens
+	 * 	Register new user
 	 */
-	@UsePipes(new ValidationPipe())
+	@ApiCreatedResponse({ description: 'Register new user' })
 	@HttpCode(200)
+	@UsePipes(new ValidationPipe())
 	@Post('register')
 	register(@Body() dto: AuthDto) {
 		return this.authService.register(dto);
 	}
-
 	/**
-	 * 	Login, getNewTokens
+	 * 	Login user
 	 */
+	@ApiCreatedResponse({ description: 'Login user' })
 	@UsePipes(new ValidationPipe())
 	@HttpCode(200)
 	@Post('login')
 	login(@Body() dto: AuthDto) {
 		return this.authService.login(dto);
 	}
+	/**
+	 * 	Update token
+	 */
+	@ApiCreatedResponse({ description: 'Update token' })
 	@UsePipes(new ValidationPipe())
 	@HttpCode(200)
 	@Post('login/access-token')
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-
 	app.setGlobalPrefix('api/v1');
+
+	const config = new DocumentBuilder()
+		.setTitle('Koala Task Manager')
+		.setDescription('The Koala API description')
+		.setVersion('1.0')
+		.addTag('Koala API')
+		.addBearerAuth()
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('swagger', app, document);
+
 	app.enableCors();
 	await app.listen(4200);
 }
